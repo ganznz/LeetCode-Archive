0ca4cec69b3bdafe843e932a12c1580ee5451e72
ganznz <loganbellinghamlb@gmail.com>
2024-06-16 22:31:05 +1200

    Solved Easy problem #217: Contains Duplicate

---
+++ easy/217-contains-duplicate.ts
@@ -0,0 +1,11 @@
+function containsDuplicate(nums: number[]): boolean {
+    const hashSet = new Set<number>();
+    for (const num of nums) {
+        if (hashSet.has(num)) {
+            return true;
+        }
+        hashSet.add(num);
+    }
+
+    return false;
+}

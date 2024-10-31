85911fb7fa64713170ad8dc191554b56de63ce10
ganznz <loganbellinghamlb@gmail.com>
2024-11-01 12:35:24 +1300

    solved 'medium' #167

---
+++ medium/167-two-sum-ii.js
@@ -0,0 +1,21 @@
+const twoSum = (arr, target) => {
+    let ptr1 = 0, ptr2 = arr.length-1;
+    let canBreak = false;
+    for (let i = 0; i <= arr.length; i++) {
+        ptr1 = i;
+        for (let j = arr.length-1; j >= 0; j--) {
+            ptr2 = j;
+            if (arr[ptr1] + arr[ptr2] < target) break;
+            if (arr[ptr1] + arr[ptr2] == target) {
+                canBreak = true;
+                break;
+            }
+        }
+        if (canBreak) break;
+    }
+
+    return [ptr1 + 1, ptr2 + 1];
+}
+
+console.log(twoSum([2,7,11,15], 9));
+console.log(twoSum([2,3,4], 6));

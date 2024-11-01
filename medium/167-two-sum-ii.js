c3e71f1fa3a01fda4e64acddb4d6c2d99556b130
ganznz <loganbellinghamlb@gmail.com>
2024-11-02 12:34:57 +1300

    re-solved 'medium' problem #167, now O(n) instead of O(n^2)

--- medium/167-two-sum-ii.js
+++ medium/167-two-sum-ii.js
@@ -1,20 +1,38 @@
-const twoSum = (arr, target) => {
-    let ptr1 = 0, ptr2 = arr.length-1;
-    let canBreak = false;
-    for (let i = 0; i <= arr.length; i++) {
-        ptr1 = i;
-        for (let j = arr.length-1; j >= 0; j--) {
-            ptr2 = j;
-            if (arr[ptr1] + arr[ptr2] < target) break;
-            if (arr[ptr1] + arr[ptr2] == target) {
-                canBreak = true;
-                break;
-            }
-        }
-        if (canBreak) break;
-    }
+// const twoSum = (arr, target) => {
+//     let ptr1 = 0, ptr2 = arr.length-1;
+//     let canBreak = false;
+//     for (let i = 0; i <= arr.length; i++) {
+//         ptr1 = i;
+//         for (let j = arr.length-1; j >= 0; j--) {
+//             ptr2 = j;
+//             if (arr[ptr1] + arr[ptr2] < target) break;
+//             if (arr[ptr1] + arr[ptr2] == target) {
+//                 canBreak = true;
+//                 break;
+//             }
+//         }
+//         if (canBreak) break;
+//     }
 
-    return [ptr1 + 1, ptr2 + 1];
+//     return [ptr1 + 1, ptr2 + 1];
+// }
+
+/*
+    REDO WITH O(n) COMPLEXITY
+*/
+
+const twoSum = (arr, target) => {
+    let ptr1 = 0;
+    let ptr2 = arr.length-1;
+    let result;
+    while (ptr1 < ptr2) {
+        result = arr[ptr1] + arr[ptr2];
+        if (result !== target) {
+            result > target ? ptr2-- : ptr1++;
+            continue;
+        }
+        return [ptr1+1, ptr2+1];
+    }
 }
 
 console.log(twoSum([2,7,11,15], 9));

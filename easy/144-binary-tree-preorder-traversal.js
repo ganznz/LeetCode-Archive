3d96685e65c153b70736079de43a2ca35d6d808b
ganznz <loganbellinghamlb@gmail.com>
2024-11-18 23:17:05 +1300

    re-solved 'easy' problem #144 to use recursion

--- easy/144-binary-tree-preorder-traversal.js
+++ easy/144-binary-tree-preorder-traversal.js
@@ -1,3 +1,5 @@
+import { TreeNode } from "../dsa-implementations/data-structures/tree/treeNode.js";
+
 /**
  * Definition for a binary tree node.
  * function TreeNode(val, left, right) {
@@ -26,3 +28,71 @@ const preorderTraversal = root => {
 
     return result;
 };
+
+/* recursive implementation */
+const preorderTraversalRecursive = (root, result = []) => {
+    if (!root) return result;
+
+    result.push(root.val);
+    if (root.left) preorderTraversalRecursive(root.left, result);
+    if (root.right) preorderTraversalRecursive(root.right, result);
+    
+    return result;
+}
+
+/*
+    test cases
+*/
+
+// 1. complete binary tree
+let root = new TreeNode(1, 
+    new TreeNode(2, 
+        new TreeNode(4), 
+        new TreeNode(5)
+    ), 
+    new TreeNode(3)
+);
+// output: [1, 2, 4, 5, 3]
+console.log(preorderTraversal(root));
+console.log(preorderTraversalRecursive(root));
+
+// 2. skewed left-heavy binary tree
+root = new TreeNode(1, 
+    new TreeNode(2, 
+        new TreeNode(3, 
+            new TreeNode(4)
+        )
+    )
+);
+// output: [1, 2, 3, 4]
+console.log(preorderTraversal(root));
+console.log(preorderTraversalRecursive(root));
+
+// 3. skewed right-heavy binary tree
+root = new TreeNode(1, 
+    null, 
+    new TreeNode(2, 
+        null, 
+        new TreeNode(3, 
+            null, 
+            new TreeNode(4)
+        )
+    )
+);
+// output: [1, 2, 3, 4]
+console.log(preorderTraversal(root));
+console.log(preorderTraversalRecursive(root));
+
+// 4. random binary tree structure
+root = new TreeNode(1, 
+    new TreeNode(2, 
+        null, 
+        new TreeNode(4)
+    ), 
+    new TreeNode(3, 
+        new TreeNode(5)
+    )
+);
+// output: [1, 2, 4, 3, 5]
+console.log(preorderTraversal(root));
+console.log(preorderTraversalRecursive(root));

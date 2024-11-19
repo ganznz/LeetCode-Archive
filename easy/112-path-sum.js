4ff22f363b41b915cdc54f130c151b599cc9000a
ganznz <loganbellinghamlb@gmail.com>
2024-11-19 19:26:03 +1300

    solved 'easy' problem #112

---
+++ easy/112-path-sum.js
@@ -0,0 +1,85 @@
+import { TreeNode } from "../dsa-implementations/data-structures/tree/treeNode.js";
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+
+const hasPathSum = (root, targetSum) => {
+    if (!root) return false;
+
+    const recurse = (root, currSum = 0) => {
+        const newVal = currSum+root.val;
+
+        if (!root.left && !root.right && newVal === targetSum) {
+            return true;
+        }
+
+        if (root.left && recurse(root.left, newVal)) return true;
+        if (root.right && recurse(root.right, newVal)) return true;
+
+        return false;
+    }
+    return recurse(root);
+}
+
+/*
+    test cases
+*/
+
+// 1. complete binary tree
+let root = new TreeNode(1, 
+    new TreeNode(2, 
+        new TreeNode(4), 
+        new TreeNode(5)
+    ), 
+    new TreeNode(3)
+);
+// output: true
+console.log(hasPathSum(root, 8));
+
+// 2. skewed left-heavy binary tree
+root = new TreeNode(1, 
+    new TreeNode(2, 
+        new TreeNode(3, 
+            new TreeNode(4)
+        )
+    )
+);
+// output: false
+console.log(hasPathSum(root, 7));
+
+// 3. skewed right-heavy binary tree
+root = new TreeNode(1, 
+    null, 
+    new TreeNode(2, 
+        null, 
+        new TreeNode(3, 
+            null, 
+            new TreeNode(4)
+        )
+    )
+);
+// output: false
+console.log(hasPathSum(root, 0));
+
+// 4. random binary tree structure
+root = new TreeNode(1, 
+    new TreeNode(2, 
+        null, 
+        new TreeNode(4)
+    ), 
+    new TreeNode(3, 
+        new TreeNode(5)
+    )
+);
+// output: true
+console.log(hasPathSum(root, 9));

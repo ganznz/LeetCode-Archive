b3edf866c23b6cc58256a822720939d6797cb955
ganznz <loganbellinghamlb@gmail.com>
2024-11-19 14:02:56 +1300

    re-solved 'easy' problem #94 to use recursion

--- easy/94-binary-tree-inorder-traversal.js
+++ easy/94-binary-tree-inorder-traversal.js
@@ -1,3 +1,5 @@
+import { TreeNode } from "../dsa-implementations/data-structures/tree/treeNode.js";
+
 /**
  * Definition for a binary tree node.
  * function TreeNode(val, left, right) {
@@ -34,3 +36,71 @@ const inorderTraversal = root => {
 
     return result;
 };
+
+/* recursive implementation */
+const inOrderRecursive = (root, result = []) => {
+    if (!root) return [];
+
+    if (root.left) inOrderRecursive(root.left, result);
+    result.push(root.val);
+    if (root.right) inOrderRecursive(root.right, result);
+    
+    return result;
+}
+
+/*
+    test cases
+*/
+
+// 1. complete binary tree
+let root = new TreeNode(1, 
+    new TreeNode(2, 
+        new TreeNode(4), 
+        new TreeNode(5)
+    ), 
+    new TreeNode(3)
+);
+// output: [4, 2, 5, 1, 3]
+console.log(inorderTraversal(root));
+console.log(inOrderRecursive(root));
+
+// 2. skewed left-heavy binary tree
+root = new TreeNode(1, 
+    new TreeNode(2, 
+        new TreeNode(3, 
+            new TreeNode(4)
+        )
+    )
+);
+// output: [4, 3, 2, 1]
+console.log(inorderTraversal(root));
+console.log(inOrderRecursive(root));
+
+// 3. skewed right-heavy binary tree
+root = new TreeNode(1, 
+    null, 
+    new TreeNode(2, 
+        null, 
+        new TreeNode(3, 
+            null, 
+            new TreeNode(4)
+        )
+    )
+);
+// output: [1, 2, 3, 4]
+console.log(inorderTraversal(root));
+console.log(inOrderRecursive(root));
+
+// 4. random binary tree structure
+root = new TreeNode(1, 
+    new TreeNode(2, 
+        null, 
+        new TreeNode(4)
+    ), 
+    new TreeNode(3, 
+        new TreeNode(5)
+    )
+);
+// output: [2, 4, 1, 5, 3]
+console.log(inorderTraversal(root));
+console.log(inOrderRecursive(root));

dc709229c00bad5f2e7e8cce10f2ca85d5a7d379
ganznz <loganbellinghamlb@gmail.com>
2025-04-13 15:43:00 +1200

    resolved 'medium' problem #209 using recursion

--- medium/209-minimum-size-subarray-sum.js
+++ medium/209-minimum-size-subarray-sum.js
@@ -19,7 +19,29 @@ const minSubArrayLen = (target, nums) => {
     return minSubArrLen === Infinity ? 0 : minSubArrLen;
 }
 
-console.log(minSubArrayLen(7, [2,3,1,2,4,3]))
-console.log(minSubArrayLen(4, [1,4,4]))
-console.log(minSubArrayLen(11, [1,1,1,1,1,1,1,1]))
-console.log(minSubArrayLen(11, [1,2,3,4,5]))
+console.log(minSubArrayLen(7, [2,3,1,2,4,3])) // output: 2
+console.log(minSubArrayLen(4, [1,4,4])) // output: 1
+console.log(minSubArrayLen(11, [1,1,1,1,1,1,1,1])) // output: 0
+console.log(minSubArrayLen(11, [1,2,3,4,5])) // output: 3
+
+// recursive implementation
+const minSubArrayLenRecursive = (target, nums, i = 0, k = 0, currSubArrSum = nums[i], minSubArrLen = Infinity) => {
+    if (i < nums.length) {
+        if (currSubArrSum >= target) {
+            minSubArrLen = Math.min(minSubArrLen, i-k+1);
+            currSubArrSum -= nums[k];
+            return minSubArrayLenRecursive(target, nums, i, k+1, currSubArrSum, minSubArrLen);
+
+        } else {
+            currSubArrSum += nums[i+=1];
+            return minSubArrayLenRecursive(target, nums, i, k, currSubArrSum, minSubArrLen);
+        }
+    }
+
+    return minSubArrLen === Infinity ? 0 : minSubArrLen;
+}
+
+console.log(minSubArrayLenRecursive(7, [2,3,1,2,4,3])) // output: 2
+console.log(minSubArrayLenRecursive(4, [1,4,4])) // output: 1
+console.log(minSubArrayLenRecursive(11, [1,1,1,1,1,1,1,1])) // output: 0
+console.log(minSubArrayLenRecursive(11, [1,2,3,4,5])) // output: 3

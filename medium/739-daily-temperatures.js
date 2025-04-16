353c0bb3cb9ecd688d55fbc37f267c0810b07606
ganznz <loganbellinghamlb@gmail.com>
2025-04-16 12:11:42 +1200

    revision: monotic stacks

--- medium/739-daily-temperatures.js
+++ medium/739-daily-temperatures.js
@@ -1,18 +1,19 @@
-// solved using a decreasing monotonic stack
-const dailyTemperatures = temperatures => {
-    const result = Array.from(temperatures, x => x = 0);
+// monotonic decreasing stack approach
+
+const dailyTemperatures = temps => {
+    const result = Array.from(temps, x => 0);
     const stack = [];
 
-    for (let i = 0; i < temperatures.length; i++) {
-        while (stack.length && temperatures[stack[stack.length-1]] < temperatures[i]) {
-            const currIndex = stack.pop();
-            result[currIndex] = i-currIndex;
+    for (let i=0; i < temps.length; i++) {
+        while (stack.length && temps[stack[stack.length-1]] < temps[i]) {
+            const curr = stack.pop();
+            result[curr] = i-curr;
         }
         stack.push(i);
     }
     return result;
 }
 
-dailyTemperatures([73,74,75,71,69,72,76,73]) // [1,1,4,2,1,1,0,0]
-dailyTemperatures([30,40,50,60]) // [1,1,1,0]
-dailyTemperatures([30,60,90]) // [1,1,0]
+console.log(dailyTemperatures([73,74,75,71,69,72,76,73])) // [1,1,4,2,1,1,0,0]
+console.log(dailyTemperatures([30,40,50,60])) // [1,1,1,0]
+console.log(dailyTemperatures([30,60,90])) // [1,1,0]
